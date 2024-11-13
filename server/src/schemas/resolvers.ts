021bb0062776c5e4e8b71f78f3ab8f68b4bcc33b
bryceberczik <berczikbryce@gmail.com>
2024-11-13 17:18:32 -0600

    beginning user testing

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,4 +1,3 @@
-import { Query } from "mongoose";
 import { User } from "../models/index.js";
 import { signToken, AuthenticationError } from "../utils/auth.js";
 
@@ -24,10 +23,10 @@ const resolvers = {
     users: async () => {
       return User.find().populate("listings").populate("jobs");
     },
-    user: async (_parent: unknown, { username }: UserArgs) => {
+    user: async (_parent: any, { username }: UserArgs) => {
         return User.findOne({ username }).populate('listings').populate('jobs')
     },
-    me: async (_parent: unknown, _args: unknown, context: any) => {
+    me: async (_parent: any, _args: any, context: any) => {
         // If the user is authenticated, find and return the user's information along with their thoughts
         if (context.user) {
           return User.findOne({ _id: context.user._id }).populate('listings').populate('jobs');
@@ -38,7 +37,35 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (_parent: unknown, { input }: AddUserArgs) => {
+        const user = await User.create({ ...input });
+        const token = signToken(user.username, user.email, user._id);
 
-    }
+        return { token, user };
+    },
+    login: async (_parent: any, { email, password }: LoginUserArgs) => {
+        // Find a user with the provided email
+        const user = await User.findOne({ email });
+      
+        // If no user is found, throw an AuthenticationError
+        if (!user) {
+          throw new AuthenticationError('Could not authenticate user.');
+        }
+      
+        // Check if the provided password is correct
+        const correctPw = await user.isCorrectPassword(password);
+      
+        // If the password is incorrect, throw an AuthenticationError
+        if (!correctPw) {
+          throw new AuthenticationError('Could not authenticate user.');
+        }
+      
+        // Sign a token with the user's information
+        const token = signToken(user.username, user.email, user._id);
+      
+        // Return the token and the user
+        return { token, user };
+      },
   }
 };
+
+export default resolvers;
