1ed1373571f12fe8586b5ac663bbb228e2ad5f02
bryceberczik <berczikbryce@gmail.com>
2024-11-15 13:46:26 -0600

    working on header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,11 +1,27 @@
+import "../css/header.css";
+
 const Header = () => {
-
-    return (
-        
-        <div>
-
+  return (
+    <div>
+      <header>
+        <div className="header-left">
+          <h1>codeBounty</h1>
         </div>
-    )
+        <div className="header-center">
+          <h1>Home</h1>
+          <h1>Explore</h1>
+          <h1>Find work</h1>
+          <h1>Post a Job</h1>
+          <h1>About</h1>
+        </div>
+        <div className="header-right">
+          <div className="signup-btn">
+            <h1>Sign up</h1>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
