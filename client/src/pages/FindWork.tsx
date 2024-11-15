7895b5e74db8924fda60bb032c5f80f3ba337546
VanceGoblin <zvkubajak@gmail.com>
2024-11-15 17:34:17 -0600

    Created findwork.css & Import React Bootstrap

--- client/src/pages/FindWork.tsx
+++ client/src/pages/FindWork.tsx
@@ -1,11 +1,12 @@
+import { Form, Button } from "react-bootstrap";
+import "../css/findwork.css";
+
 const FindWork = () => {
-
-    return (
-
-        <div>
-
-        </div>
-    )
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default FindWork;
