2246d3b3188c4bb33b92fa5b793f18edad3a4d6b
VanceGoblin <zvkubajak@gmail.com>
2024-11-15 17:34:42 -0600

    Created Search Bar w/ RB

--- client/src/pages/FindWork.tsx
+++ client/src/pages/FindWork.tsx
@@ -4,7 +4,17 @@ import "../css/findwork.css";
 const FindWork = () => {
   return (
     <div>
-      
+      <h1>Search for work that fits your role.</h1>
+
+      <Form className="d-flex">
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
     </div>
   );
 };
