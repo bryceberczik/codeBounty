7f714c8458a92437a00ba570f941e6fbdb5a7b54
bryceberczik <berczikbryce@gmail.com>
2024-11-16 13:40:02 -0600

    fixed minor typo on footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -3,7 +3,7 @@ import '../css/footer.css';
 const Footer = () => {
   return <div className='footer-container'>
     <footer>
-    <p>&copy; 2024 codyBounty. All rights reserved.</p>
+    <p>&copy; 2024 codeBounty. All rights reserved.</p>
     <p>Project created for educational purposes and is not intended for commercial use.</p>
     <p>Built by Bryce Berczik, Justin Hebenstreit, Zander Kubajak, and Jarvis Young.</p>
     </footer>
