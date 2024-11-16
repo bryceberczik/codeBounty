b205c063dac6944b04d8e8105cfeb7738f8a178e
bryceberczik <berczikbryce@gmail.com>
2024-11-16 10:58:49 -0600

    fix sign up button, and reworked css class no-link

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
     <p>&copy; 2024 codyBounty. All rights reserved.</p>
     <p>Project created for educational purposes and is not intended for commercial use.</p>
-    <p>Built by Bryce Berczik, Justin HebenStreit, Zander Kubajak, and Jarvis Young.</p>
+    <p>Built by Bryce Berczik, Justin Hebenstreit, Zander Kubajak, and Jarvis Young.</p>
     </footer>
   </div>;
 };
