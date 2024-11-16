93f959e9a9ec1912de0632996a3d754d1b505f3c
VanceGoblin <zvkubajak@gmail.com>
2024-11-15 19:50:05 -0600

    Added Positioning for Multiple Listing Cards

--- client/src/components/ListingCard.tsx
+++ client/src/components/ListingCard.tsx
@@ -3,23 +3,21 @@ import "../css/listingcard.css";
 
 const ListingCard = () => {
   return (
-    <div>
-      <Card id="listing-card">
-        <Card.Body>
-          <Card.Title id="card-title">Hello World</Card.Title>
-          <Card.Subtitle id="card-lister" className="mb-2">
-            Posted by: Username
-          </Card.Subtitle>
-          <Card.Text id="card-description">
-            <strong>Description: </strong>This is where the listing description
-            goes.
-          </Card.Text>
-          <Card.Text id="card-price">
-            <strong>$100</strong>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card id="listing-card">
+      <Card.Body>
+        <Card.Title id="card-title">Hello World</Card.Title>
+        <Card.Subtitle id="card-lister" className="mb-2">
+          Posted by: Username
+        </Card.Subtitle>
+        <Card.Text id="card-description">
+          <strong>Description: </strong>This is where the listing description
+          goes.
+        </Card.Text>
+        <Card.Text id="card-price">
+          <strong>$100</strong>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
--- client/src/pages/FindWork.tsx
+++ client/src/pages/FindWork.tsx
@@ -1,3 +1,4 @@
+import { Container, Row, Col } from "react-bootstrap";
 import { Form, Button } from "react-bootstrap";
 import ListingCard from "../components/ListingCard";
 import "../css/findwork.css";
@@ -9,7 +10,7 @@ const FindWork = () => {
 
       <Form id="find-work-search" className="d-flex">
         <Form.Control
-					id="find-work-search-bar"
+          id="find-work-search-bar"
           type="search"
           placeholder="Search"
           aria-label="Search"
@@ -19,7 +20,19 @@ const FindWork = () => {
         </div>
       </Form>
 
-			<ListingCard />
+      <Container id="listings-container">
+        <Row>
+          <Col md={4} sm={6}>
+            <ListingCard />
+          </Col>
+          <Col md={4} sm={6}>
+            <ListingCard />
+          </Col>
+          <Col md={4} sm={6}>
+            <ListingCard />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
