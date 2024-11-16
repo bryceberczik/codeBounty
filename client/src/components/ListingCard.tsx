3a0b6319cee1f1640fa9606de3df7d0cda232993
VanceGoblin <zvkubajak@gmail.com>
2024-11-15 21:19:24 -0600

    Created Apply Button for Listing Card

--- client/src/components/ListingCard.tsx
+++ client/src/components/ListingCard.tsx
@@ -1,4 +1,4 @@
-import Card from "react-bootstrap/Card";
+import { Card, Button } from "react-bootstrap";
 import "../css/listingcard.css";
 
 interface ListingCardProps {
@@ -15,21 +15,27 @@ const ListingCard = ({
   price,
 }: ListingCardProps) => {
   return (
-    <Card id="listing-card">
-      <Card.Body>
-        <Card.Title id="card-title">{title}</Card.Title>
-        <Card.Subtitle id="card-lister" className="mb-2">
-          Posted by: {poster}
-        </Card.Subtitle>
-        <Card.Text id="card-description">
-          <strong>Description: </strong>
-          {description}
-        </Card.Text>
-        <Card.Text id="card-price">
-          <strong>${price}</strong>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div style={{ position: "relative" }}>
+      <Card id="listing-card">
+        <Card.Body>
+          <Card.Title id="card-title">{title}</Card.Title>
+          <Card.Subtitle id="card-lister" className="mb-2">
+            Posted by: {poster}
+          </Card.Subtitle>
+          <Card.Text id="card-description">
+            <strong>Description: </strong>
+            {description}
+          </Card.Text>
+          <Card.Text id="card-price">
+            <strong>${price}</strong>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Button id="apply-button">
+        Apply
+      </Button>
+    </div>
   );
 };
 
