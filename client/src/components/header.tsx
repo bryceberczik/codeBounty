068a56e34fde99b0de981496c2917267b9308e99
bryceberczik <berczikbryce@gmail.com>
2024-11-16 18:07:29 -0600

    completed first section of explore

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -10,32 +10,61 @@ const Header = () => {
           <h1>codeBounty</h1>
         </div>
         <div className="header-center">
-          <Link
-            to="/" className="no-underline">
-            <h1 className={currentPage === "/" ? "activeNav" : "restNav"}>Home</h1>
-          </Link>
-          <Link
-            to="/explore" className="no-underline">
-            <h1 className={currentPage === "/explore" ? "activeNav" : "restNav"}>Explore</h1>
-          </Link>
-          <Link
-            to="/find-work" className="no-underline">
-            <h1 className={currentPage === "/find-work" ? "activeNav" : "restNav"}>Find work</h1>
-          </Link>
-          <Link
-            to="/post-listing" className="no-underline">
-            <h1 className={currentPage === "/post-listing" ? "activeNav" : "restNav"}>Post a job</h1>
-          </Link>
-          <Link
-            to="/about-us" className="no-underline">
-            <h1 className={currentPage === "/about-us" ? "activeNav" : "restNav"}>About</h1>
-          </Link>
+          <div className="category-item">
+            <Link to="/" className="no-underline">
+              <h1 className={currentPage === "/" ? "activeNav" : "restNav"}>
+                Home
+              </h1>
+            </Link>
+          </div>
+          <div className="category-item">
+            <Link to="/explore" className="no-underline">
+              <h1
+                className={currentPage === "/explore" ? "activeNav" : "restNav"}
+              >
+                Explore
+              </h1>
+            </Link>
+          </div>
+          <div className="category-item">
+            <Link to="/find-work" className="no-underline">
+              <h1
+                className={
+                  currentPage === "/find-work" ? "activeNav" : "restNav"
+                }
+              >
+                Find work
+              </h1>
+            </Link>
+          </div>
+          <div className="category-item">
+            <Link to="/post-listing" className="no-underline">
+              <h1
+                className={
+                  currentPage === "/post-listing" ? "activeNav" : "restNav"
+                }
+              >
+                Post a job
+              </h1>
+            </Link>
+          </div>
+          <div className="category-item">
+            <Link to="/about-us" className="no-underline">
+              <h1
+                className={
+                  currentPage === "/about-us" ? "activeNav" : "restNav"
+                }
+              >
+                About
+              </h1>
+            </Link>
+          </div>
         </div>
         <div className="header-right">
           <Link to="/signup" className="no-link">
-          <div className="signup-btn">
-            <h1>Sign up</h1>
-          </div>
+            <div className="signup-btn">
+              <h1>Sign up</h1>
+            </div>
           </Link>
         </div>
       </header>
