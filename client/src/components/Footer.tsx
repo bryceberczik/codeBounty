26553a13dc43061f4cf8797901bea89feb1efbed
J H <j.hebenstreit25@gmail.com>
2024-11-18 19:46:06 -0600

    almost done debugging pages

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import "../css/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <footer>
+        <p>&copy; 2024 codeBounty. All rights reserved.</p>
+        <p>
+          Project created for educational purposes and is not intended for
+          commercial use.
+        </p>
+        <p>
+          Built by Bryce Berczik, Justin Hebenstreit, Zander Kubajak, and Jarvis
+          Young.
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
