20409122d882826d0a77c39927512374a5f42393
bryceberczik <berczikbryce@gmail.com>
2024-11-21 13:00:17 -0600

    completed the modal design for viewing applicants

--- client/src/components/YourListingCard.tsx
+++ client/src/components/YourListingCard.tsx
@@ -237,7 +237,7 @@ const YourListingCard = ({
                 <div className="applicant-container" key={index}>
                   <h4>{applicant.username}</h4>
                   <div className="applicant-profile-btn">
-                    <Link to={`/profiles/${applicant.username}`}>
+                    <Link style={{ textDecoration: "none", color: "#003049" }} to={`/profiles/${applicant.username}`}>
                       <p>{applicant.username}'s Profile</p>
                     </Link>
                   </div>
