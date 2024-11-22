029d87b0fc57b31ee3fbd26bef140abaaae37a51
J H <j.hebenstreit25@gmail.com>
2024-11-21 20:21:39 -0600

    added error page stylings

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,18 +1,31 @@
 import { useRouteError } from "react-router-dom";
+import "../css/errorPage.css";
 
 export default function errorPage() {
   const error: any = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
+    <div className="errorPage">
+      {/* <h1>Oops!</h1>
 
       <p>Sorry, an unexpected error has occured.</p>
 
       <p>
         <i>{error.status || error.message}</i>
-      </p>
+      </p> */}
+
+      <h1 className="noBananas">404 | Banana Not Found!!</h1>
+
+      <iframe
+        src="https://giphy.com/embed/H8uuXYln5pxVVLFn7k"
+        height="270"
+        frameBorder="0"
+        allowFullScreen
+        title="Bananas GIF"
+        className="banana"
+      ></iframe>
+
     </div>
   );
 }
