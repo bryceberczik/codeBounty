d8f9dc3782d1589d619f0dba26e2e70c89165f08
VanceGoblin <zvkubajak@gmail.com>
2024-11-22 20:05:05 -0600

    Removed Old console.logs to Cut Down on File Size (16 lines)

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -2,14 +2,12 @@ import { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import { UPDATE_USER } from "../utils/mutations";
-
-import { useParams } from "react-router-dom";
-import Auth from "../utils/auth";
-
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import { Button, Form } from "react-bootstrap";
 import { FaRegEdit } from "react-icons/fa";
 import PageTab from "../components/PageTab";
+import { useParams } from "react-router-dom";
+import Auth from "../utils/auth";
 import "../css/userprofile.css";
 
 interface IUserProfile {
@@ -27,16 +25,12 @@ const UserProfile = ({ username }: { username?: string }) => {
   // * Gets a username from either the URL params (user's profile) or the logged-in user's token (my profile).
   const displayedUsername = username || paramsUsername || loggedInUser;
 
-  // console.log(displayedUsername);
-
   if (!displayedUsername) {
     return <p>Error: No username available.</p>;
   }
 
   // * isOwnProfile is a boolean that checks if the profile being visited is the logged-in user's profile. When this is true, the user gets access to the editing tools.
   const isOwnProfile = loggedInUser === username;
-
-  // if (isOwnProfile) console.log(isOwnProfile);
 
   const { loading, data } = useQuery(isOwnProfile ? QUERY_ME : QUERY_USER, {
     variables: isOwnProfile ? undefined : { username: displayedUsername },
@@ -45,8 +39,6 @@ const UserProfile = ({ username }: { username?: string }) => {
 
   // * userId for UPDATE_USER mutation.
   const userId = data?.me?._id;
-
-  // console.log(userId);
 
   const [updateUser] = useMutation(UPDATE_USER);
 
@@ -60,8 +52,6 @@ const UserProfile = ({ username }: { username?: string }) => {
 
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
   const [alertVariant, setAlertVariant] = useState<string>("success");
-
-  // console.log(user);
 
   useEffect(() => {
     if (data?.me || data?.user) {
@@ -197,10 +187,6 @@ const UserProfile = ({ username }: { username?: string }) => {
       document.getElementById("description-profile-input")?.innerText ||
       user?.description;
 
-    // console.log(usernameInput);
-    // console.log(roleInput);
-    // console.log(descriptionInput);
-
     if (usernameInput.length > 20) {
       setAlertMessage("Your username cannot exceed 20 characters.");
       setAlertVariant("danger");
@@ -234,8 +220,6 @@ const UserProfile = ({ username }: { username?: string }) => {
       technologies: user.technologies,
       links: user.links,
     };
-
-    // console.log(updatedUserData);
 
     updateUser({
       variables: {
