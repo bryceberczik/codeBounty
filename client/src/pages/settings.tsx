93194643d9f0b3e27db6bf82a163d4659112134e
VanceGoblin <zvkubajak@gmail.com>
2024-11-23 20:01:36 -0600

    Created Starting Delete User Logic

--- client/src/pages/settings.tsx
+++ client/src/pages/settings.tsx
@@ -1,6 +1,44 @@
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { DELETE_USER } from "../utils/mutations";
+import { Modal, Button } from "react-bootstrap";
 import "../css/settings.css";
 
 const Settings = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const [deleteUser] = useMutation(DELETE_USER);
+  const { loading, data } = useQuery(QUERY_ME);
+  const user = data?.me;
+
+  const handleShowModal = async () => {
+    try {
+      const userId = user._id;
+      console.log(userId);
+
+      handleDeleteUser(userId);
+    } catch (error) {
+      console.error("handleShowModal Error:", error);
+    }
+  };
+
+  // const handleCloseModal = () => setShowModal(false);
+
+  const handleDeleteUser = async (userId: string) => {
+    console.log(userId);
+
+    await deleteUser({
+      variables: {
+        id: userId,
+      },
+    });
+
+    alert(`Your account has been deleted. Hope to see you again soon!`);
+  };
+
+  if (loading) return <p style={{ paddingBottom: "1000px" }}>Loading...</p>;
+
   return (
     <div className="settings-div">
       <h1>Settings</h1>
@@ -14,11 +52,11 @@ const Settings = () => {
         </div>
         <div className="setting-opt">
           <h3>Account Deletion</h3>
-          <button className="delete-user-btn">Delete Account</button>
+          <button onClick={() => handleShowModal()} className="delete-user-btn">
+            Delete Account
+          </button>
         </div>
       </div>
-
-      
     </div>
   );
 };
